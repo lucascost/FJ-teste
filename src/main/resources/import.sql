a7ad2f935bd8d5c8df7999c762327054d6df064b
lucascost <lcyancosta@gmail.com>
2023-09-18 10:47:24 -0300

    :sparkles: feat: data seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO produto (nome, preco, qtd_em_estoque) VALUES('Pão Hot-dog', 7.9, 30);
+INSERT INTO produto (nome, preco, qtd_em_estoque) VALUES('Pão De Forma tradicional', 7.5, 30);
+INSERT INTO produto (nome, preco, qtd_em_estoque) VALUES('Pão De Forma integral', 9.2, 30);
